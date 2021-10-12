9344e13b17331fc5e82f1163ac754797aa05322c
vanessa <hpj766@my.utsa.edu>
2021-10-11 21:29:41 -0500

    Edited return types of game interface functions

--- src/Game.java
+++ src/Game.java
@@ -2,8 +2,8 @@ public interface Game {
     public void startGame();
     public void playTurn();
     public void declareWar();
-    public void isGameDone();
-    public void getWinner();
+    public boolean isGameDone();
+    public player getWinner();
     public void setTurn(int turnNum);
-    public void getTurn();
+    public int getTurn();
 }
