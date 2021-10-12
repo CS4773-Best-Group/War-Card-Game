ab6a052e6fd355ae73c2bede2be1e0be998066ee
vanessa <hpj766@my.utsa.edu>
2021-10-11 22:37:17 -0500

    Wrote playTurn function

--- src/game/GameVar1.java
+++ src/game/GameVar1.java
@@ -1,12 +1,11 @@
 package game;
 
-import models.Deck;
+import models.Card;
 import models.Player;
 
 public class GameVar1 implements Game {
     Player player1;
     Player player2;
-    Deck deck;
     int turn;
     int maxTurns;
 
@@ -14,6 +13,7 @@ public class GameVar1 implements Game {
         this.player1 = player1;
         this.player2 = player2;
         this.maxTurns = maxTurns;
+        // deal cards to player 1 and 2
     }
 
     @Override
@@ -25,7 +25,20 @@ public class GameVar1 implements Game {
 
     @Override
     public void playTurn() {
-        // if (player1.playHand());
+        Card card1 = player1.playHand();
+        Card card2 = player2.playHand();
+
+        if (card1.getRank() == card2.getRank()) {
+            declareWar();
+        } else if (card1.getRank().compareTo(card2.getRank()) > 0) { // player 1 wins
+            player1.addToHand(card1);
+            player1.addToHand(card2);
+            player1.addPoints(2);
+        } else { // player 2 wins
+            player2.addToHand(card1);
+            player2.addToHand(card2);
+            player2.addPoints(2);
+        }
     }
 
     @Override
