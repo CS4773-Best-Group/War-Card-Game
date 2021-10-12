27704a5ec17cd8621930e4692badf0bea6ba215b
Bethany Salazar <bethanysalazar2007@hotmail.com>
2021-10-11 22:30:41 -0500

    COMPLETE: dealCards

--- src/Deck.java
+++ src/Deck.java
@@ -22,8 +22,12 @@ public class Deck {
     // TODO: not sure if we are sending in the correct information, returning a card for the hands or return a queue of cards
     public void dealCards(Player p1, Player p2) {
         // TODO: split deck into 2 and deal two players
-        p1.setHand.addAll(deck.subList(0, 25));
-        p2.setHand.addAll(deck.subList(26, deck.size()));
+        Queue<Card> p1Hand = null;
+        Queue<Card> p2Hand = null;
+        p1Hand.addAll(deck.subList(0, 25));
+        p2Hand.addAll(deck.subList(26, deck.size()));
+        p1.setHand(p1Hand);
+        p2.setHand(p2Hand);
     }
 
     /**
@@ -36,7 +40,7 @@ public class Deck {
     boolean isEmpty() {
         // TODO: ret true if all cards have been dealt (deck is empty)
         if (getCardsCount() == 0) {
-            return true
+            return true;
         }
         return false;
     }
