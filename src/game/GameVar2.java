740aea54b30c7d3e7560ae2f3f000c5b1298674b
vanessa <hpj766@my.utsa.edu>
2021-10-11 21:58:19 -0500

    IMPORTANT: PUT FILES INTO PACKAGES

--- src/game/GameVar2.java
+++ src/game/GameVar2.java
@@ -1,21 +1,22 @@
-public class GameVar1 implements Game {
+package game;// IMPORTANT NOTE: THIS CLASS MAY NEED TO GET BROKEN UP DEPENDING ON SIZE OF FILE
+
+import models.Deck;
+import models.Player;
+
+public class GameVar2 implements Game {
     Player player1;
     Player player2;
     Deck deck;
     int turn;
-    int maxTurns;
 
-    public GameVar1(Player player1, Player player2, int maxTurns) {
+    public GameVar2(Player player1, Player player2) {
         this.player1 = player1;
         this.player2 = player2;
-        this.maxTurns = maxTurns;
     }
 
     @Override
     public void startGame() {
-        while (!isGameDone()) {
-            playTurn();
-        }
+        // TODO
     }
 
     @Override
@@ -31,11 +32,13 @@ public class GameVar1 implements Game {
     @Override
     public boolean isGameDone() {
         // TODO
+        return false;
     }
 
     @Override
     public Player getWinner() {
         // TODO
+        return null;
     }
 
     @Override
@@ -46,5 +49,6 @@ public class GameVar1 implements Game {
     @Override
     public int getTurn() {
         // TODO
+        return 0;
     }
 }
