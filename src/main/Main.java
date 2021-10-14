25f2c243c15d485e80f686878bb307bac7b9e6e6
Bethany Salazar <bethanysalazar2007@hotmail.com>
2021-10-14 18:38:14 -0500

    Added some argument validation in main, wont be useful until we are doing command line arguments

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,12 +1,21 @@
 package main;
 
 import game.Game;
+import game.GameVar1;
 import game.GameVar2;
 import models.Player;
 
 public class Main {
     public static void main(String[] args) {
         // TODO: create and run game based on args
+        if (args.length < 1 || args.length > 2){
+            throw new IllegalArgumentException("Invalid number of arguments");
+        }
+        int gameVariation = Integer.parseInt(args[0]);
+        int maxRounds = Integer.parseInt(args[1]);
+        if (gameVariation == 1){
+            Game game = new GameVar1(new Player(1), new Player(2), maxRounds);
+        }
         Game game = new GameVar2(new Player(1), new Player(2));
         System.out.println("starting game");
         game.startGame();
