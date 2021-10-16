c39ae01b39f30b5ed7abf77a75dd8f859f70e6ee
Vanessa <hpj766@my.utsa.edu>
2021-10-15 21:52:40 -0500

    Fixed output

--- src/game/GameVar2.java
+++ src/game/GameVar2.java
@@ -1,4 +1,4 @@
-package game;// IMPORTANT NOTE: THIS CLASS MAY NEED TO GET BROKEN UP DEPENDING ON SIZE OF FILE
+package game;
 import models.Deck;
 import models.Player;
 import models.Card;
@@ -15,20 +15,19 @@ public class GameVar2 implements Game {
         pointsCounter = 0;
         deck = new Deck();
         deck.shuffleDeck();
-        System.out.println("DEALING CARDS ========");
         deck.dealCards(player1, player2);
-//        System.out.println("p1: " + player1.getNumCardsInHand());
-//        System.out.println("p2: " + player2.getNumCardsInHand());
     }
 
     @Override
     public void startGame() {
         while (!isGameDone()){
             turn += 1;
-            System.out.println("turn " + getTurn() + " =====================");
             playTurn();
         }
-        System.out.println("WINNER: " + getWinner().getPlayerNum());
+        if (getWinner() == null) {
+            System.out.println("Game has ended in a TIE!");
+        }
+        System.out.println("player " + getWinner().getPlayerNum() + " WINS the game!!!");
     }
 
     @Override
@@ -36,24 +35,28 @@ public class GameVar2 implements Game {
         Card card1 = player1.playHand();
         Card card2 = player2.playHand();
         pointsCounter += 2;
-        System.out.println("P1 PLAYED: " + card1);
-        System.out.println("P2 PLAYED: " + card2);
+        System.out.println("Player 1 plays " + card1.toString());
+        System.out.println("Player 2 plays " + card2.toString());
         if(card1.getRank() == card2.getRank() && !isGameDone()){
             declareWar();
         } else if (card1.getRank().compareTo(card2.getRank()) > 0){
             player1.addPoints(pointsCounter);
-            System.out.println("player 1 wins | " + player1.getPoints() + "-" + player2.getPoints());
+            System.out.println("Player 1 wins the round");
+            System.out.println("Player 1 has a score of " + player1.getPoints());
+            System.out.println("Player 2 has a score of " + player2.getPoints());
             pointsCounter = 0;
         } else {
             player2.addPoints(pointsCounter);
-            System.out.println("player 2 wins | " + player1.getPoints() + "-" + player2.getPoints());
+            System.out.println("Player 2 wins the round");
+            System.out.println("Player 1 has a score of " + player1.getPoints());
+            System.out.println("Player 2 has a score of " + player2.getPoints());
             pointsCounter = 0;
         }
     }
 
     @Override
     public void declareWar() {
-        System.out.println("WAR");
+        System.out.println("*** WAR! ***");
         if (player1.getNumCardsInHand() > 1 || player2.getNumCardsInHand() > 1) {
             player1.playHand();
             player2.playHand();
@@ -72,6 +75,9 @@ public class GameVar2 implements Game {
 
     @Override
     public Player getWinner() {
+        if (player1.getPoints() == player2.getPoints()) {
+            return null;
+        }
         if (player1.getPoints() > player2.getPoints()) {
             return player1;
         }
--- src/main/Main.java
+++ src/main/Main.java
@@ -8,8 +8,6 @@ public class Main {
     public static void main(String[] args) {
         // TODO: create and run game based on args
         Game game = new GameVar2(new Player(1), new Player(2));
-        System.out.println("starting game");
         game.startGame();
-        System.out.println("game done");
     }
 }
